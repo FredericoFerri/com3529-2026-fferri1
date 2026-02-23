1f69cf78932221d4b43e8156e679bca54204a3fa
FredericoFerri <ferrifrederico040@gmail.com>
2026-02-23 22:45:29 +0000

    Inclusion of DIFFixed.java, containing the correction of the for loop to access the first content of the list. Changes in the DIFTest to use methods from DIFFixed class

--- code/lib/src/test/java/uk/ac/shef/com3529/DIFTest.java
+++ code/lib/src/test/java/uk/ac/shef/com3529/DIFTest.java
@@ -14,50 +14,50 @@ public class DIFTest {
 
     @Test
     public void shouldReturnIndexZeroWhenElementIsFirst() {
-        assertEquals(0, DIF.findLast(new int[]{5, 1, 2, 3}, 5));
+        assertEquals(0, DIFFixed.findLast(new int[]{5, 1, 2, 3}, 5));
     }
 
     @Test
     public void shouldReturnCorrectIndexWhenElementIsInMiddle() {
-        assertEquals(2, DIF.findLast(new int[]{1, 2, 3, 4}, 3));
+        assertEquals(2, DIFFixed.findLast(new int[]{1, 2, 3, 4}, 3));
     }
 
     @Test
     public void shouldReturnLastIndexWhenElementIsLast() {
-        assertEquals(3, DIF.findLast(new int[]{1, 2, 3, 4}, 4));
+        assertEquals(3, DIFFixed.findLast(new int[]{1, 2, 3, 4}, 4));
     }
 
     // ===== Multiple occurrence cases =====
 
     @Test
     public void shouldReturnLastIndexWhenOccurrencesAreSeparated() {
-        assertEquals(3, DIF.findLast(new int[]{1, 2, 1, 1}, 1));
+        assertEquals(3, DIFFixed.findLast(new int[]{1, 2, 1, 1}, 1));
     }
 
     @Test
     public void shouldReturnLastIndexWhenOccurrencesAreAdjacent() {
-        assertEquals(2, DIF.findLast(new int[]{2, 3, 3, 4}, 3));
+        assertEquals(2, DIFFixed.findLast(new int[]{2, 3, 3, 4}, 3));
     }
 
     @Test
     public void shouldReturnIndexOneWhenFirstTwoElementsMatch() {
-        assertEquals(1, DIF.findLast(new int[]{7, 7, 3, 4}, 7));
+        assertEquals(1, DIFFixed.findLast(new int[]{7, 7, 3, 4}, 7));
     }
 
     @Test
     public void shouldReturnMinusOneWhenElementNotPresent() {
-        assertEquals(-1, DIF.findLast(new int[]{1, 2, 3, 4}, 5));
+        assertEquals(-1, DIFFixed.findLast(new int[]{1, 2, 3, 4}, 5));
     }
 
     @Test
     public void shouldthrowNullPointerExceptionWhenArrayIsNull() {
         assertThrows(NullPointerException.class, () -> 
-            DIF.findLast(null, 5)
+            DIFFixed.findLast(null, 5)
         );
     }
 
     @Test
     public void shouldReturnMinusOneWhenArrayIsEmpty() {
-        assertEquals(-1, DIF.findLast(new int[]{}, 5));
+        assertEquals(-1, DIFFixed.findLast(new int[]{}, 5));
     }
 }
